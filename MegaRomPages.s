935bd5141f28868aaa66c166257a960cce419990
Andre Baptista <albs_br@yahoo.com.br>
2025-04-18 10:13:50 -0300

    v.0.36.1

--- MegaRomPages.s
+++ MegaRomPages.s
@@ -35,6 +35,7 @@ MEGAROM_PAGE_FRAME_HEADERS: equ 2
     INCLUDE "Data/subzero/jumping-up/right/subzero_jumping_up_right_frame_headers.s"
     ; INCLUDE "Data/subzero/jumping-forward/right/subzero_jumping_forward_right_frame_headers.s"
 
+MegaRom_Page_2_size: equ $ - 0x8000 ; 0x37ec
 
 	ds PageSize - ($ - 0x8000), 255
 
@@ -46,6 +47,8 @@ MEGAROM_PAGE_SCORPION_DATA_0: equ 3
 
     INCLUDE "Data/scorpion/stance/left/scorpion_stance_left_frames_0_to_6_data_and_list.s"
 
+MegaRom_Page_3_size: equ $ - 0x8000 ; 0x31a1
+
 	ds PageSize - ($ - 0x8000), 255
 
 ; ------------------------------------------------- Page 4 -------------------------------------------------
@@ -56,7 +59,7 @@ MEGAROM_PAGE_SUBZERO_DATA_0: equ 4
 
     INCLUDE "Data/subzero/stance/right/subzero_stance_right_frames_0_to_8_data_and_list.s"
 
-MegaROM_Page_4_size: equ $ - 0x8000
+MegaRom_Page_4_size: equ $ - 0x8000 ; 0x3cd5
 
 	ds PageSize - ($ - 0x8000), 255
 
@@ -68,6 +71,8 @@ MEGAROM_PAGE_SUBZERO_DATA_1: equ 5
 
     INCLUDE "Data/subzero/stance/right/subzero_stance_right_frames_9_to_12_data_and_list.s"
 
+MegaRom_Page_5_size: equ $ - 0x8000 ; 0x1afd
+
 	ds PageSize - ($ - 0x8000), 255
 
 ; ------------------------------------------------- Page 6 -------------------------------------------------
@@ -77,6 +82,8 @@ MEGAROM_PAGE_SUBZERO_WALKING_RIGHT_DATA_0: equ 6
 	org	0x8000, 0xBFFF
 
     INCLUDE "Data/subzero/walking/right/subzero_walking_right_frames_0_to_8_data_and_list.s"
+
+MegaRom_Page_6_size: equ $ - 0x8000 ; 0x3a2b
 
 	ds PageSize - ($ - 0x8000), 255
 
@@ -88,6 +95,8 @@ MEGAROM_PAGE_SCORPION_WALKING_LEFT_DATA_0: equ 7
 
     INCLUDE "Data/scorpion/walking/left/scorpion_walking_left_frames_0_to_8_data_and_list.s"
 
+MegaRom_Page_7_size: equ $ - 0x8000 ; 0x3a2b
+
 	ds PageSize - ($ - 0x8000), 255
 
 ; ------------------------------------------------- Page 8 -------------------------------------------------
@@ -97,17 +106,15 @@ MEGAROM_PAGE_SCORPION_JUMPING_FORWARD_LEFT_DATA_0: equ 8
 
 	org	0x8000, 0xBFFF
 
-; ------------------------------------------------------------------------
-
-    ; Data
+    ; ------------ Data ---------------
 	INCLUDE "Data/scorpion/jumping-up/left/scorpion_jumping_up_left_frames_0_to_2_data.s"
 	INCLUDE "Data/scorpion/jumping-forward/left/scorpion_jumping_forward_left_frames_0_to_7_data.s"
 
-; ------------------------------------------------------------------------
-
-    ; List
+    ; ------------ List ---------------
     INCLUDE "Data/scorpion/jumping-up/left/scorpion_jumping_up_left_frames_0_to_2_data_and_list.s"
     INCLUDE "Data/scorpion/jumping-forward/left/scorpion_jumping_forward_left_frames_0_to_7_data_and_list.s"
+
+MegaRom_Page_8_size: equ $ - 0x8000 ; 0x2bcd
 
 	ds PageSize - ($ - 0x8000), 255
 
@@ -118,16 +125,14 @@ MEGAROM_PAGE_SUBZERO_JUMPING_FORWARD_RIGHT_DATA_0: equ 9
 
 	org	0x8000, 0xBFFF
 
-; ------------------------------------------------------------------------
-
-    ; Data
+    ; ------------ Data ---------------
 	INCLUDE "Data/subzero/jumping-up/right/subzero_jumping_up_right_frames_0_to_2_data.s"
 	; INCLUDE "Data/subzero/jumping-forward/right/subzero_jumping_forward_right_frames_0_to_7_data.s"
 
-; ------------------------------------------------------------------------
-
-    ; List
+    ; ------------ List ---------------
     INCLUDE "Data/subzero/jumping-up/right/subzero_jumping_up_right_frames_0_to_2_data_and_list.s"
     ; INCLUDE "Data/subzero/jumping-forward/right/subzero_jumping_forward_right_frames_0_to_7_data_and_list.s"
+
+MegaRom_Page_9_size: equ $ - 0x8000
 
 	ds PageSize - ($ - 0x8000), 255
