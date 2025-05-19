56e6d29d0b4fedbc2cb58bc270df4e4bce04a96d
Andre Baptista <albs_br@yahoo.com.br>
2025-05-19 19:52:04 -0300

    v.0.49.1

--- Data/scorpion/block/left/scorpion_block_left_frame_0_header.s
+++ Data/scorpion/block/left/scorpion_block_left_frame_0_header.s
@@ -1,2 +1,2 @@
 ;		yOffset	width	height	megaROM page	List Address
-	dw	128	db	50,	104,	MEGAROM_PAGE_SCORPION_BLOCK_LEFT_DATA_0	dw	Scorpion_Block_Left_Frame_0.List
+	dw	256	db	60,	104,	MEGAROM_PAGE_SCORPION_BLOCK_LEFT_DATA_0	dw	Scorpion_Block_Left_Frame_0.List
--- Data/scorpion/block/left/scorpion_block_left_frame_2_header.s
+++ Data/scorpion/block/left/scorpion_block_left_frame_2_header.s
@@ -1,2 +1,2 @@
 ;		yOffset	width	height	megaROM page	List Address
-	dw	256	db	42,	104,	MEGAROM_PAGE_SCORPION_BLOCK_LEFT_DATA_0	dw	Scorpion_Block_Left_Frame_2.List
+	dw	384	db	52,	104,	MEGAROM_PAGE_SCORPION_BLOCK_LEFT_DATA_0	dw	Scorpion_Block_Left_Frame_2.List
