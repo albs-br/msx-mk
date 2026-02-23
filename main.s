0868fc378b6771c97212afc5e3d327cbe4813708
Andre Baptista <albs_br@yahoo.com.br>
2026-02-23 16:05:42 -0300

    v.0.60.12

--- main.s
+++ main.s
@@ -454,6 +454,31 @@ Palette:
     INCBIN "Images/mk.pal"
 ; TODO: change color 4 of the bg to color 0, so this color 4 can be used to red (blood/lifebars)
 
+
+; ----- bg palette usage (goro's lair scenario):
+
+; Size: 30336 bytes
+; Number of pixels: 60608
+; Lines: 236.75
+; Color 0: 21097 pixels (34.81%)
+; Color 1: 10569 pixels (17.44%)
+; Color 2: 5606 pixels (9.25%)
+; Color 3: 8056 pixels (13.29%)
+; Color 4: 5129 pixels (8.46%)
+; Color 5: 2002 pixels (3.30%)
+; Color 6: 4495 pixels (7.42%)
+; Color 7: 861 pixels (1.42%)
+; Color 8: 878 pixels (1.45%)
+; Color 9: 1490 pixels (2.46%)
+; Color 10: 214 pixels (0.35%)
+; Color 11: 0 pixels (0.00%)        scorpion/subzero
+; Color 12: 6 pixels (0.01%)        scorpion/subzero/lifebars
+; Color 13: 0 pixels (0.00%)        subzero
+; Color 14: 203 pixels (0.33%)      scorpion/subzero
+; Color 15: 2 pixels (0.00%)        scorpion/lifebars/eyes on cave
+; Unique colors: 14
+
+
 ; --------------------------------------------------------
 
 ; size: 90x12 pixels
